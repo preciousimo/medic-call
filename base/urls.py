55e57208e41f76b4b8ffe4a6a5d79db04bfdbf44
preciousimo <preciousimoniakemu@gmail.com>
2022-03-03 20:14:51 +0100

    Login system

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    
     path('', views.frontend, name="frontend"),
     path('backend/', views.backend, name="backend"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import authenticate, login, logout
+# from .models import User
+# from .forms import MyUserCreationForm
 # Create your views here.
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('backend')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+    # context = {'page': page}
+    return render(request, 'base/registration/login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('frontend')
+
+
 def frontend(request):
     return render(request, "base/frontend.html")
 
+@login_required(login_url='login')
 def backend(request):
     return render(request, "base/backend.html")
