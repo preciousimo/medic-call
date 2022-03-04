acfcbdd63f14b29c3f6c59a683aa619da0b385a4
preciousimo <preciousimoniakemu@gmail.com>
2022-03-04 17:49:20 +0100

    Fix bug in login system

--- base/views.py
+++ base/views.py
@@ -3,22 +3,32 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth import authenticate, login, logout
-# from .models import User
-# from .forms import MyUserCreationForm
+from django.contrib.auth.models import User
+from base.models import Patient 
 # Create your views here.
 
 def loginPage(request):
     page = 'login'
     if request.user.is_authenticated:
         return redirect('backend')
-
+    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
         user = authenticate(request, username=username, password=password)
 
-    # context = {'page': page}
+        if user is not None:
+            login(request, user)
+            return redirect('backend')
+        else:
+            messages.error(request, 'Username OR password does not exit')
+
     return render(request, 'base/registration/login.html')
 
 
@@ -33,3 +43,19 @@ def frontend(request):
 @login_required(login_url='login')
 def backend(request):
     return render(request, "base/backend.html")
+
+@login_required(login_url='login')
+def add_patient(request):
+    if request.method == 'POST':
+        if request.POST.get('name') and request.POST.get('phone') and request.POST.get('email') and request.POST.get('age') and request.POST.get('gender' ) or request.POST.get('note'):
+            patient = Patient()
+            patient.name = request.POST.get('name')
+            patient.phone = request.POST.get('phone')
+            patient.email = request.POST.get('email')
+            patient.age = request.POST.get('age')
+            patient.gender = request.POST.get('gender')
+            patient.note = request.POST.get('note')
+            patient.save()
+            messages.success(request, "Patient added successfully!")
+    else:
+        return render(request, "base/add.html")
