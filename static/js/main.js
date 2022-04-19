a9bff151e24daa4eda1bafc046eaea80cc62cf83
preciousimo <preciousimoniakemu@gmail.com>
2022-04-19 12:18:28 +0100

    Set dynamic time

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function() {
         $("#no-data").text("No patient found");
     }
 });
+
+// Time running at realtime
+setInterval(function() {
+    var date = new Date();
+    $("#clock").html(
+        (date.getHours() < 10 ? '0' : '') + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ":" + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds()
+    );
+}, 500);
