b588a4dcdee703f281a1941a56e3e39ff50dcc1b
preciousimo <preciousimoniakemu@gmail.com>
2022-08-24 15:52:35 +0100

    update

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import  HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
 from django.views.decorators.cache import cache_control
