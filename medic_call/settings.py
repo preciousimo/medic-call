d1b29b5dcd48e312e26a2ee3c912a45668e660c2
preciousimo <preciousimoniakemu@gmail.com>
2022-09-23 10:09:57 +0100

    Update!!

--- medic_call/settings.py
+++ medic_call/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 from decouple import config
 
